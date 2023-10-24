cc81a9da657ab0e150f82dad1a57312646506e6e
Liam Segura <liamdiegosegura@gmail.com>
2023-10-24 14:21:11 +0100

    added search to confluence page, to be used as an example

--- src/app/(routes)/confluence/page.tsx
+++ src/app/(routes)/confluence/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useDebounce from "@/app/hooks/useDebounce";
 import React, { useState, useEffect } from "react";
 
 interface ConfluenceResult {
@@ -10,7 +11,19 @@ interface ConfluenceResult {
 
 function ConfluencePage() {
   const [confluenceData, setConfluenceData] = useState<ConfluenceResult[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortedData, setSortedData] = useState<ConfluenceResult[]>([]);
+  const [selectedResult, setSelectedResult] = useState<ConfluenceResult | null>(
+    null
+  );
 
+  const handleResultClick = (result: ConfluenceResult) => {
+    setSelectedResult(result);
+  };
+
+  const debouncedSearchQuery = useDebounce(searchQuery, 300);
+
+  // Fetch the data from the API
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,6 +31,7 @@ function ConfluencePage() {
         if (response.ok) {
           const data = await response.json();
           setConfluenceData(data);
+          setSortedData(data);
           console.log("response ok?");
         } else {
           console.error(
@@ -33,42 +47,93 @@ function ConfluencePage() {
     fetchData();
   }, []);
 
-  console.log("confluenceData", confluenceData);
+  // Sort the data based on the search query
+  useEffect(() => {
+    const sorted = confluenceData.slice().sort((a, b) => {
+      const aStartsWithQuery = a.title
+        .toLowerCase()
+        .startsWith(debouncedSearchQuery.toLowerCase());
+      const bStartsWithQuery = b.title
+        .toLowerCase()
+        .startsWith(debouncedSearchQuery.toLowerCase());
 
-  return (
-    <div className="flex">
+      if (aStartsWithQuery && !bStartsWithQuery) {
+        return -1;
+      } else if (!aStartsWithQuery && bStartsWithQuery) {
+        return 1;
+      } else {
+        return a.title.localeCompare(b.title);
+      }
+    });
+
+    setSortedData(sorted);
+  }, [confluenceData, debouncedSearchQuery]);
+
+  // Render the selected result
+  function SelectedResultDetails({ result }: { result: ConfluenceResult }) {
+    return (
       <div>
-        {confluenceData ? (
-          <ul className="space-y-4">
-            {confluenceData.map((result) => (
-              <li key={result.id} className="p-4">
-                <h3 className="text-2xl font-semibold text-gray-700">
-                  {result.title}
-                </h3>
-              </li>
-            ))}
+        {result ? (
+          <ul className="w-full">
+            <li key={result.id} className="p-4">
+              <h3 className="text-2xl font-semibold text-gray-700 mb-1">
+                {result.title}
+              </h3>
+              <p
+                dangerouslySetInnerHTML={{ __html: result.innerHTML }}
+                className="text-lg text-gray-500 leading-relaxed"
+              />
+            </li>
           </ul>
         ) : (
           <p>No results found</p>
         )}
       </div>
-      <div>
-        {confluenceData ? (
-          <ul className="space-y-4">
-            {confluenceData.map((result) => (
-              <li key={result.id} className="p-4 rounded-lg shadow-md">
-                <h3 className="text-2xl font-semibold text-gray-700 mb-1">
-                  {result.title}
-                </h3>
-                <p
-                  dangerouslySetInnerHTML={{ __html: result.innerHTML }}
-                  className="text-lg text-gray-500 leading-relaxed"
-                />
-              </li>
+    );
+  }
+
+  return (
+    <div className="flex">
+      <div className="basis-0">
+        <input
+          type="text"
+          placeholder="Search by title"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-fill p-2 mb-4 border rounded-lg"
+        />
+
+        {searchQuery ? (
+          <ul className="w-full">
+            {sortedData.slice(0, 20).map((result) => (
+              <h3
+                className="text-2xl font-semibold text-gray-700 mb-1 cursor-pointer hover:text-gray-500"
+                onClick={() => handleResultClick(result)}
+              >
+                {result.title}
+              </h3>
             ))}
           </ul>
         ) : (
-          <p>No results found</p>
+          <ul className="w-full">
+            {sortedData.map((result) => (
+              <h3
+                className="text-2xl font-semibold text-gray-700 mb-1 cursor-pointer hover:text-gray-500"
+                onClick={() => handleResultClick(result)}
+              >
+                {result.title}
+              </h3>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="pt-10 flex justify-center w-full">
+        {selectedResult ? (
+          <SelectedResultDetails result={selectedResult} />
+        ) : (
+          <h3 className="text-2xl font-semibold text-gray-700 mb-1">
+            Search Glossary And Click a Result
+          </h3>
         )}
       </div>
     </div>
